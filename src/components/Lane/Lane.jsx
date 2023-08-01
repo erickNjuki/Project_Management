e2cb667e7405e71c918bdbd4faa69a4a229844a7
erickNjuki <njukierick@gmail.com>
2023-08-01 23:12:24 +0300

    Loading and displaying data

--- src/components/Lane/Lane.jsx
+++ src/components/Lane/Lane.jsx
@@ -1,9 +1,17 @@
+import Task from "../Task/Task";
 import "./Lane.css";
 
-function Lane({ title }) {
+function Lane({ title, loading, error, tasks }) {
   return (
     <div className="Lane-wrapper">
       <h2>{title}</h2>
+      {loading || error ? (
+        <span>{error || "Loading..."}</span>
+      ) : (
+        tasks.map((tasks) => (
+          <Task key={tasks.id} key={tasks.title} key={tasks.body} />
+        ))
+      )}
     </div>
   );
 }
