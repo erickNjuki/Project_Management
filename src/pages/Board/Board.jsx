4f37e7b7cc60b7a832bc6fcef8a8fccf6c49584e
erickNjuki <njukierick@gmail.com>
2023-08-02 10:33:53 +0300

    Made the Board dynamic with the OnDrag event

--- src/pages/Board/Board.jsx
+++ src/pages/Board/Board.jsx
@@ -1,3 +1,4 @@
+import { useEffect, useState } from "react";
 import useDataFetching from "../../hooks/useDataFetching";
 import Lane from "../../components/Lane/Lane";
 import "./Board.css";
@@ -9,20 +10,50 @@ const lanes = [
   { id: 4, title: "Done" },
 ];
 
+function onDragStart(e, id) {
+  e.dataTransfer.setData("id", id);
+}
+
+function onDragOver(e) {
+  e.preventDefault();
+}
+
 function Board() {
-  const [loading, error, tasks] = useDataFetching(
+  const [loading, error, data] = useDataFetching(
     `https://my-json-server.typicode.com/PacktPublishing/React-Projects-Second-Edition/tasks`
   );
+
+  const [tasks, setTasks] = useState([]);
+
+  useEffect(() => {
+    setTasks(data);
+  }, [data]);
+
+  function onDrop(e, laneId) {
+    const id = e.dataTransfer.getData("id");
+
+    const updatedTasks = tasks.filter((task) => {
+      if (task.id.toString() === id) {
+        task.lane = laneId;
+      }
+      return task;
+    });
+
+    setTasks(updatedTasks);
+  }
 
   return (
     <div className="Board-wrapper">
       {lanes.map((lane) => (
         <Lane
           key={lane.id}
+          laneId={lane.id}
           title={lane.title}
           loading={loading}
           error={error}
           tasks={tasks.filter((task) => task.lane === lane.id)}
+          onDragStart={onDragStart}
+          onDragOver={onDragOver}
         />
       ))}
     </div>
